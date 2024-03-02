1f2d02a0593094374a6e3072cb9e39277a9405ec
Raju Kumar <rajuk9586@gmail.com>
2024-03-02 12:52:22 +0530

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Raju Kumar on 02/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
